7fa1166083067819746346e2205fe817eb932170
Michal Bojanowski <michal2992@gmail.com>
2013-11-20 18:27:55 +0100

    Updating cb
    Now using write_cb()
    Documenting

--- R/cb.R
+++ R/cb.R
@@ -1,49 +1,56 @@
 #' Exporting R objects to clipboard
 #' 
-#' This function exports R objects into Windows clipboard in a format that
-#' makes the further work in MS Office easier. Current methods cover matrices
-#' and data frames.
+#' This function exports R objects into the system clipboard in a format that
+#' is easy to paste it into Office-like programs (MSWord etc.).  Currently,
+#' methods for vectors, matrices, tables and data frames are implemented. For
+#' other objects the function tries to coerce its argument to a matrix first.
+#'
+#' An object is converted to a tab-separated table first. Details of the
+#' conversion depend on the class, see below. Such converted object is written
+#' into the system clipboard. A message stating the size of the exported table
+#' is printed into the console. Content of the clipboard can then be pasted in
+#' to MS Word or MS Excel, or any other application. 
+#'
+#' For making tables in MSWord, LibreOffice, or alike you will have to first
+#' create an empty table with appropriate number of rows and columns. To aid
+#' you in this, when \code{cb} is called an informative message about the
+#' dimensionality of the exported table is printed. As a next step you have to
+#' select the whole table in MS Word, preferably via menus: Table|Select|Table.
+#' Then paste the content of the clipboard.
 #' 
-#' There are currently three methods: for matrices, tables, and data frames.
-#' Data frames and tables are converted to matrices. \code{tcb} is an alias for
-#' \code{cbd}.
-#' 
-#' For any other objects the function tries to coerce its argument to matrix.
-#' 
+#' In MS Excel and alike you are not required to select the appropriate area of
+#' the spreadsheet. It is sufficient to put the cursor (cell selector) in the
+#' upper-left corner of the to-be-table.
+#'
 #' If \code{useNames} is \code{TRUE} then the object is expanded to contain row
 #' and column names. The first cell in the first row contain dimension names
-#' separated with a backslash.
-#' 
-#' This function converts the object to a tab-separated table and puts it in
-#' the Windows clipboard. The content can then be pasted in to MS Word or MS
-#' Excel. For making tables in MS Word you will have to first create an empty
-#' table with appropriate number of rows and columns. To aid you in this after
-#' the \code{cb} is called it displays an informative message about the
-#' dimensionality of the exported table. As a next step you have to select the
-#' whole table in MS Word, preferably via menus: Table|Select|Table.  Then
-#' paste in the content of the clipboard.
-#' 
-#' In MS Excel you are not required to select the appropriate area of the
-#' spread sheet. It is sufficient to put the cursor (cell selector) in the
-#' upper-left corner of the to-be-table.
-#' 
-#' @param object object to processed, currently matrix, data frame or table are
-#' supported directly. Other objects are coerced to matrix.
+#' (\code{names(dimnames(object))}) separated with a backslash.
+#'
+#' Writing to clipboard is performed using \code{\link{write_cb}}.
+#'
+#' @param object object to be processed
 #' @param useNames logical, whether to use dimension names
-#' @param \dots other arguments to methods
+#' @param ... other arguments to/from other methods
 #'
 #' @return The converted object is put to clipboard and returned invisibly.
 #'
-#' @note This function will work only on MS Windows distributions of R
-#'
-#' @seealso \code{writeClipboard} and package \pkg{xtable}
+#' @seealso \code{\link{write_cb}}, section Clipboard of \code{?file}, package
+#' \pkg{xtable} for exporting R objects to LaTeX or HTML.
 #'
 #' @export
-#'
 cb <- function(object, ...) UseMethod("cb")
 
 
-cb.matrix <- function(object, useNames=TRUE)
+#' @method cb matrix
+#' @export
+#' @rdname cb
+#' @details
+#' If \code{object} is a matrix, the content is pasted into a single string in
+#' which columns are separated with tabs and rows with newline characters.
+#' Depending on the value of \code{useNames} row and column names are added as
+#' initial row and column to the result. If \code{object}'s dimnames have names
+#' defined, they are put in the first cell of the first row.
+cb.matrix <- function(object, useNames=getOption("clipboard.useNames", TRUE), ...)
 {
 	if( !useNames | is.null(dimnames(object)) ) {
 	    m <- object
@@ -59,8 +66,7 @@ cb.matrix <- function(object, useNames=TRUE)
 	}
 	rval <- apply( m, 1, paste, collapse="\t")
 	rval <- paste(rval, collapse="\n")
-	require(utils)
-	writeClipboard(rval)
+	write_cb(rval, ...)
 	cat("In clipboard:",
 	    paste(dim(m), collapse=" x "),
 	    "table\n")
@@ -68,40 +74,56 @@ cb.matrix <- function(object, useNames=TRUE)
 }
 
 
-cb.data.frame <- function(object, useNames=TRUE)
+#' @method cb data.frame
+#' @export
+#' @rdname cb
+#' @details
+#' If \code{object} is a data frame it is converted to a matrix processed as
+#' such. If the data frame contains factors, they are converted to chacracter
+#' strings first.
+cb.data.frame <- function(object, ...)
 {
 	# convert factors two character
 	isf <- sapply(object, is.factor)
 	d <- object
 	d[isf] <- lapply( d[isf], as.character )
 	# process as character matrix
-	cb( as.matrix(d), useNames=useNames)
+	cb( as.matrix(d), ...)
 }
 
 
-# for tables
-cb.table <- function(object, useNames=TRUE)
+#' @method cb table
+#' @export
+#' @rdname cb
+#' @details
+#' If \code{object} is a table it is converted to matrix. Tables with more than two
+#' dimensions are not supported. One-dimensional tables are exported horizontally.
+cb.table <- function(object, ...)
 {
-    # only if table is one- or two-dimensional
-    ndim <- length(dim(object))
-    if( ndim > 2 )
-	stop("'object' has ", ndim, " dimensions, only 1 or 2 supported")
-    # horizontal table for one dimension
-    if( ndim == 1 )
-    {
-	rval <- cb( t(as.matrix(object)), useNames=useNames )
-    } else
-    {
-	class(object) <- "matrix"
-	rval <- cb( object, useNames=useNames )
-    }
-    invisible(rval)
+  # only if table is one- or two-dimensional
+  ndim <- length(dim(object))
+  if( ndim > 2 )
+    stop("'object' has ", ndim, " dimensions, only 1 or 2 supported")
+  # horizontal table for one dimension
+  if( ndim == 1 )
+  {
+    rval <- cb( t(as.matrix(object)), ... )
+  } else
+  {
+    class(object) <- "matrix"
+    rval <- cb( object, ... )
+  }
+  invisible(rval)
 }
 
 
-# Default method tries to coerce the object to matrix
-
-cb.default <- function(object, useNames=TRUE)
+#' @method cb default
+#' @export
+#' @rdname cb
+#' @details
+#' For all other types of \code{object}'s not covered above, \code{cb} tries to
+#' convert them to a matrix and process it as such.
+cb.default <- function(object, ...)
 {
-    cb( as.matrix(object), useNames=useNames )
+    cb( as.matrix(object), ... )
 }
