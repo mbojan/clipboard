2cc7ddb484b90f50396a0ad7bfa195144b3c6717
Michal Bojanowski <michal2992@gmail.com>
2014-09-11 16:00:14 +0200

    On linux presence of xclip is checked on loading

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname)
+{
+  if( Sys.info()["sysname"] == "Linux" )
+  {
+    if( !has_xclip() )
+      warning("it seems that xclip is not installed, package 'clipboard' may not work correctly")
+  }
+}
