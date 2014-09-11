5ae191bdfb937d590e5121a01a234bc08272a3e9
Michal Bojanowski <michal2992@gmail.com>
2014-09-11 16:26:09 +0200

    On loading on linux checking for xclip

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,10 @@
   if( Sys.info()["sysname"] == "Linux" )
   {
     if( !has_xclip() )
+    {
       warning("it seems that xclip is not installed, package 'clipboard' may not work correctly")
+    } else {
+      message(has_xclip(TRUE), " found")
+    }
   }
 }
