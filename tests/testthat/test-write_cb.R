b62b70f0e610dfcf8c24f709536a9e58fadf53be
Michal Bojanowski <michal2992@gmail.com>
2016-11-17 09:52:14 +0100

    Skipping tests if no xclip on linux

--- tests/testthat/test-write_cb.R
+++ tests/testthat/test-write_cb.R
@@ -1,16 +1,22 @@
 context("Low-level writing and reading to/from clipboard")
 
 test_that("Character vector is correctly written and read", {
-          ch <- "ala ma kota"
-          write_cb(ch)
-          r <- read_cb()
-          expect_identical(ch, r)
+  
+  skip_if_not( Sys.info()[["sysname"]] != "linux" && has_xclip() )
+  
+  ch <- "ala ma kota"
+  write_cb(ch)
+  r <- read_cb()
+  expect_identical(ch, r)
 } )
 
 
 test_that("Numeric vector is correctly written and read", {
-          x <- 1:5
-          write_cb(x)
-          r <-read_cb()
-          expect_identical(as.character(x), r)
+  
+  skip_if_not( Sys.info()[["sysname"]] != "linux" && has_xclip() )
+  
+  x <- 1:5
+  write_cb(x)
+  r <-read_cb()
+  expect_identical(as.character(x), r)
 } )
